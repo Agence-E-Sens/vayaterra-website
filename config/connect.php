2d2b1a207d7073920373cd83c54aaf2857c3e948
milounis <milounis@gmail.com>
2016-05-27 12:57:34 +0200

    comit lounis

--- config/connect.php
+++ config/connect.php
@@ -2,6 +2,6 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 define('_MYSQL_SET_SQL_MODE',true);
 $GLOBALS['spip_connect_version'] = 0.7;
-spip_connect_db('localhost','','root','root','vayaterra','mysql', 'spip','');
+spip_connect_db('localhost','','root','','vayaterra','mysql', 'spip','');
 //spip_connect_db('localhost','','vayaterra','PrJ4cdnl15','vayaterra','mysql', 'spip','');
 ?>
